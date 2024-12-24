7fe9454443661d34e464fbb878bb92e7860491b5
Amndprsl <amandapricilia88@gmail.com>
2024-12-24 09:51:20 +0700

    Add files via upload

---
+++ viewnav.php
@@ -0,0 +1,32 @@
+<div class="collapse navbar-collapse order-3" id="navbarCollapse">
+    <!-- Left navbar links -->
+
+</div>
+
+
+</nav>
+<!-- /.navbar -->
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0"> Top Navigation <small>Example 3.0</small></h1>
+                </div><!-- /.col -->
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item"><a href="#">Layout</a></li>
+                        <li class="breadcrumb-item active">Top Navigation</li>
+                    </ol>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+
+    <aside class="control-sidebar control-sidebar-dark">
+        <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
